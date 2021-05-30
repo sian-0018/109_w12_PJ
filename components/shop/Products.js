8685411bbc5ec22d26a6e52c00164358fc7df51b
sian-0018 <sian-0018@gms.tku.edu.tw>
2021-05-30 15:56:04 +0800

    053004

--- components/shop/Products.js
+++ components/shop/Products.js
@@ -7,12 +7,10 @@ export default function Nav({categories,category}) {
 	return (
 		<div>
 			<h1 className={styles.products_header__1iU36}>{title} Page</h1>
-
 			<div className={styles.center}>
 			{categories.map(({name: title, remote_url:  imageUrl, cid, size,cat_id,price})=>(
                 <Item key={cid} title={title} imageUrl={imageUrl} size={size} cat_id={cat_id} price={price} />
             ))}
-				
 			</div>
 		</div>
 	);
