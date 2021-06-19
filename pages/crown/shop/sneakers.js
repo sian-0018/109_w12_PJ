b27e72a7cfec00fe3f4ac08c1940324418faeb1a
sian-0018 <sian-0018@gms.tku.edu.tw>
2021-06-20 01:49:02 +0800

    set shop product api

--- pages/crown/shop/sneakers.js
+++ pages/crown/shop/sneakers.js
@@ -1,9 +1,10 @@
 
 import Shop from '../../../components/shop/Products'
 import Nav from '../../../components/layout/Nav'
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(){
-  const response = await fetch('https://dry-wave-60207.herokuapp.com/api/shop/sneakers');
+  const response = await fetch('https://crownstrapi-demo.herokuapp.com/products');
   const categories = await response.json();
 
   return{
@@ -14,10 +15,20 @@ export async function getServerSideProps(){
 }
 
 export default function Home({categories}) {
+  const router = useRouter();
+  const filterCatId = router.query.cat_id;
+  const cat_id = (router.query.cat_id === 'sneakers' ? 0 : 3);
+
+
+  console.log('filterCatId',filterCatId);
+
+  // console.log('cat_id',cat_id);
+  const objDetail = categories.filter((obj) => obj.category.id === cat_id);
+
 	return (
 		<div >
             <Nav />
-			<Shop key={categories[0].cid} categories={categories}  category='SNEAKERS'/>
+			<Shop key={objDetail[0].cid} categories={objDetail} category='Sneakers'/>
 		</div>
 	);
 }
